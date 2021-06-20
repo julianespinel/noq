e1848bf645695f5e71d52c287068bc0eaff3f902
Julian Espinel <julianespinel@gmail.com>
2021-06-19 20:47:41 -0500

    Add integration tests for creating a Company

---
+++ src/test/java/com/jespinel/noq/NoqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jespinel.noq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
