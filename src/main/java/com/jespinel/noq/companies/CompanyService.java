eb06d926711ebee3e05aa0c1bab1c31e15219486
Julian Espinel <julianespinel@gmail.com>
2021-06-22 22:51:00 -0500

    Make service accessible from other services

--- src/main/java/com/jespinel/noq/companies/CompanyService.java
+++ src/main/java/com/jespinel/noq/companies/CompanyService.java
@@ -1,25 +1,26 @@
 package com.jespinel.noq.companies;
 
+import com.jespinel.noq.common.exceptions.DuplicatedEntityException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
-class CompanyService {
+public class CompanyService {
 
     private final CompanyRepository repository;
 
     @Autowired
-    CompanyService(CompanyRepository repository) {
+    public CompanyService(CompanyRepository repository) {
         this.repository = repository;
     }
 
-    Company create(Company company) throws DuplicatedCompanyException {
+    Company create(Company company) {
         try {
             return repository.save(company);
         } catch (DuplicateKeyException e) {
             String errorMessage = "A company with nit %s already exists".formatted(company.getNit());
-            throw new DuplicatedCompanyException(errorMessage);
+            throw new DuplicatedEntityException(errorMessage);
         }
     }
 }
