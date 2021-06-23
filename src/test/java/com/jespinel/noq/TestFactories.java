bae6df09c1fb8e47eb4d96fa3a7754975af0eb8d
Julian Espinel <julianespinel@gmail.com>
2021-06-22 23:13:23 -0500

    Create factory methods to ease test cases creation

---
+++ src/test/java/com/jespinel/noq/TestFactories.java
@@ -0,0 +1,27 @@
+package com.jespinel.noq;
+
+import com.github.javafaker.Faker;
+import com.jespinel.noq.branches.Branch;
+import com.jespinel.noq.branches.CreateBranchRequest;
+import com.jespinel.noq.companies.Company;
+
+public class TestFactories {
+
+    private static final Faker faker = new Faker();
+
+    public static Company getRandomCompany() {
+        String nit = faker.idNumber().valid();
+        String name = faker.name().name();
+        return new Company(nit, name);
+    }
+
+    public static Branch getRandomBranch(long parentId) {
+        String name = faker.name().name();
+        return new Branch(name, parentId);
+    }
+
+    public static CreateBranchRequest getCreateBranchRequest(long parentId) {
+        Branch branch = getRandomBranch(parentId);
+        return new CreateBranchRequest(branch.getName(), branch.getParentId());
+    }
+}
