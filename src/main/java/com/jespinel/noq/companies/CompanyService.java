bb7b7c8af15cfeb83b14962a2f99c7b1c1a3aceb
Julian Espinel <julianespinel@gmail.com>
2021-06-23 22:48:35 -0500

    Get a company or throw an exception if it doesn't exist

--- src/main/java/com/jespinel/noq/companies/CompanyService.java
+++ src/main/java/com/jespinel/noq/companies/CompanyService.java
@@ -1,9 +1,12 @@
 package com.jespinel.noq.companies;
 
 import com.jespinel.noq.common.exceptions.DuplicatedEntityException;
+import com.jespinel.noq.common.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CompanyService {
@@ -15,12 +18,21 @@ public class CompanyService {
         this.repository = repository;
     }
 
-    Company create(Company company) {
+    public Company create(Company company) {
         try {
             return repository.save(company);
         } catch (DuplicateKeyException e) {
             String errorMessage = "A company with nit %s already exists".formatted(company.getNit());
             throw new DuplicatedEntityException(errorMessage);
         }
+    }
+
+    public Company getOrThrow(long companyId) {
+        Optional<Company> company = repository.find(companyId);
+        company.orElseThrow(() -> {
+            String errorMessage = "The company with ID %s was not found".formatted(companyId);
+            throw new EntityNotFoundException(errorMessage);
+        });
+        return company.get();
     }
 }
