907bab2bdf8e65b61a8faee94c2633e18990019b
Julian Espinel <julianespinel@gmail.com>
2021-06-24 21:18:14 -0500

    Define queues table schema

--- src/main/resources/db/changelog/changes/v_0.0.1.sql
+++ src/main/resources/db/changelog/changes/v_0.0.1.sql
@@ -21,3 +21,16 @@ CREATE TABLE branches
     UNIQUE (name, companyId)
 );
 --rollback drop table branches;
+
+--changeset julianespinel:3
+CREATE TABLE queues
+(
+    id         SERIAL PRIMARY KEY,
+    name       VARCHAR(256) NOT NULL,
+    branchId   INTEGER REFERENCES branches (id),
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL,
+
+    UNIQUE (name, branchId)
+);
+--rollback drop table queues;
