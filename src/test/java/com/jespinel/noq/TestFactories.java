2128bccedf37503a069cf83bb1c812638121607b
Julian Espinel <julianespinel@gmail.com>
2021-07-25 19:36:08 -0400

    Add factory methods for queues and requests

--- src/test/java/com/jespinel/noq/TestFactories.java
+++ src/test/java/com/jespinel/noq/TestFactories.java
@@ -6,6 +6,8 @@ import com.jespinel.noq.branches.CreateBranchRequest;
 import com.jespinel.noq.companies.Company;
 import com.jespinel.noq.queues.CreateQueueRequest;
 import com.jespinel.noq.queues.Queue;
+import com.jespinel.noq.turns.CreateTurnRequest;
+import com.jespinel.noq.turns.TurnNumber;
 
 public class TestFactories {
 
@@ -31,17 +33,36 @@ public class TestFactories {
         return CreateBranchRequest.from(branch);
     }
 
+    public static Queue getRandomQueue() {
+        int branchId = faker.number().randomDigitNotZero();
+        String name = faker.name().name();
+        TurnNumber initialTurn = TurnNumber.from("A0");
+        return new Queue(name, initialTurn, branchId);
+    }
+
     public static Queue getRandomQueue(long branchId) {
         String name = faker.name().name();
-        return new Queue(name, branchId);
+        TurnNumber turnNumber = TurnNumber.from("A0");
+        return new Queue(name, turnNumber, branchId);
+    }
+
+    public static Queue getRandomQueue(long branchId, String initialTurn) {
+        String name = faker.name().name();
+        TurnNumber turnNumber = TurnNumber.from(initialTurn);
+        return new Queue(name, turnNumber, branchId);
     }
 
     public static CreateQueueRequest getCreateQueueRequest(long branchId) {
         Queue queue = getRandomQueue(branchId);
-        return new CreateQueueRequest(queue.getName(), queue.getBranchId());
+        String initialTurn = "A0";
+        return new CreateQueueRequest(queue.getName(), initialTurn, queue.getBranchId());
     }
 
     public static CreateQueueRequest getCreateQueueRequest(Queue queue) {
         return CreateQueueRequest.from(queue);
+    }
+
+    public static CreateTurnRequest getCreateTurnRequest(String phoneNumber, long queueId) {
+        return new CreateTurnRequest(phoneNumber, queueId);
     }
 }
