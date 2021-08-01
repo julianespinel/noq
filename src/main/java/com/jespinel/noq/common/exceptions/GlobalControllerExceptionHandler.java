0a3257733fb31f5be2f41d0fca6281026ab9e7c0
Julian Espinel <julianespinel@gmail.com>
2021-07-31 22:06:31 -0400

    Define how to handle EmptyQueueException

--- src/main/java/com/jespinel/noq/common/exceptions/GlobalControllerExceptionHandler.java
+++ src/main/java/com/jespinel/noq/common/exceptions/GlobalControllerExceptionHandler.java
@@ -33,4 +33,11 @@ public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHan
         ApiError apiError = new ApiError(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(apiError);
     }
+
+    @ExceptionHandler(EmptyQueueException.class)
+    public ResponseEntity<ApiError> handleValidationErrors(EmptyQueueException exception) {
+        logger.error(exception.getMessage(), exception);
+        ApiError apiError = new ApiError(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(apiError);
+    }
 }
