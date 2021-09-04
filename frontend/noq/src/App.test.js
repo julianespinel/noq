6e695aa2de8f735ad91a3d221e721c82fb6606ef
Julian Espinel <julianespinel@gmail.com>
2021-09-04 14:37:20 -0400

    Fix test case

--- frontend/noq/src/App.test.js
+++ frontend/noq/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Please log in/i);
   expect(linkElement).toBeInTheDocument();
 });
