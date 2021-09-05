8cc12c82c44d4764c73b8cb2e6c89dcf32c8e9db
Julian Espinel <julianespinel@gmail.com>
2021-09-05 07:44:38 -0400

    Get turnNumber from state instead of props

--- frontend/noq/src/customers/TurnConfirmation.js
+++ frontend/noq/src/customers/TurnConfirmation.js
@@ -6,24 +6,27 @@ class TurnConfirmation extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            turnNumber: props.location.state.turnNumber
+        }
+
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event) {
+    async handleSubmit(event) {
         event.preventDefault();
-        this.props.history.push("/turns");
+        await this.props.history.push("/turns");
     }
 
     render() {
-        const turnNumber = this.props.location.state.turnNumber;
-
         return (
             <main className="form-signin">
                 <form onSubmit={this.handleSubmit}>
                     <h1 className="h3 mb-3 fw-normal">Confirmation</h1>
 
                     <p>
-                        Your turn is number {turnNumber}, we will send you a SMS when you can come back.
+                        Your turn is number {this.state.turnNumber}, we will send you a SMS when you can come back.
                     </p>
 
                     <button className="w-100 btn btn-lg btn-primary" type="submit" autoFocus>Ok</button>
