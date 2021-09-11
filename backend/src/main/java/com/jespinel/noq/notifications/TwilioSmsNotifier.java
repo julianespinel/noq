16d18227695446084e1f61e20f98c503d779f544
Julian Espinel <julianespinel@gmail.com>
2021-09-11 11:39:57 -0400

    Break line to improve code readability

--- backend/src/main/java/com/jespinel/noq/notifications/TwilioSmsNotifier.java
+++ backend/src/main/java/com/jespinel/noq/notifications/TwilioSmsNotifier.java
@@ -29,7 +29,9 @@ public class TwilioSmsNotifier implements ISmsNotifier {
     @Override
     public void send(String phoneNumber, String message) {
         PhoneNumber to = new PhoneNumber(phoneNumber);
-        Message sentMessage = Message.creator(to, sender, message).create();
+        Message sentMessage = Message
+                .creator(to, sender, message)
+                .create();
         logger.debug("TWILIO: Message sent to %s, message sid %s".
                 formatted(phoneNumber, sentMessage.getSid()));
     }
